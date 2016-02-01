75972b9f9c6d8837c9a18ff693b84fa3653c34d8
TonyChen <chenyongsuda@gmail.com>
2016-02-01 18:41:02 +0800

    Update server.go
    Package The Connect to UserConnect

--- BaseLanguage/GO/ChatServer/src/server.go
+++ BaseLanguage/GO/ChatServer/src/server.go
@@ -7,6 +7,7 @@ import (
 	"io"
 	"net"
 	"runtime"
+	"time"
 )
 
 const (
@@ -37,40 +38,82 @@ func server(listen *net.TCPListener) {
 			continue
 		}
 		fmt.Println("Connect Accepted~")
-		go handConnection(conn)
+		user_conn := NewUserConnect(conn)
+		go user_conn.LoopMessage()
+		go TestMap()
 	}
 }
 
-func handConnection(conn net.Conn) {
-	defer conn.Close()
+func TestMap() {
+	for {
+		fmt.Println(GetConnectMap())
+		time.Sleep(time.Second * 3)
+	}
+}
+
+//-------------------------------------------------------------UserConnect-------------------------------------------------------------
+//init con map data
+var ConnectMap = make(map[net.Conn]int)
+
+func GetConnectMap() map[net.Conn]int {
+	return ConnectMap
+}
+
+func AddConnect(conn net.Conn) {
+	ConnectMap[conn] = 1
+}
+
+func RemoveConnect(conn net.Conn) {
+	delete(ConnectMap, conn)
+}
+
+func NewUserConnect(conn net.Conn) *UserConnect {
+	AddConnect(conn)
+	return &UserConnect{conn: conn}
+}
+
+type UserConnect struct {
+	disconnct bool
+	conn      net.Conn
+}
+
+func (uc *UserConnect) LoopMessage() {
+	defer uc.conn.Close()
 	//Read Buffer
 	read_buff := make([]byte, 4096)
 	//Read Buff Cache
 	msg_buff := bytes.NewBuffer(make([]byte, 0, 10240))
 	len := 0
 	for {
-		c, err := conn.Read(read_buff)
+		c, err := uc.conn.Read(read_buff)
 		//message, err := reader.ReadString('\n')
 		if err == io.EOF {
+			RemoveConnect(uc.conn)
 			fmt.Println("Client Exit", err.Error())
 			break
 		}
 		if err != nil {
+			RemoveConnect(uc.conn)
 			fmt.Println("Data read Error", err.Error())
 			break
 		}
 		msg_buff.Write(read_buff[:c])
 
-		handMessage(msg_buff, &len)
+		uc.handMessage(msg_buff, &len)
+
+		//disconnct flag
+		if true == uc.disconnct {
+			RemoveConnect(uc.conn)
+			break
+		}
 	}
 }
 
-func handMessage(msg_buff *bytes.Buffer, len *int) {
+func (uc *UserConnect) handMessage(msg_buff *bytes.Buffer, len *int) {
 	msg_head_msg_len := uint32(0)
 	for {
 		//Read Header
 		if *len == 0 && msg_buff.Len() >= 4 {
-			//binary.Read(msg_buff, binary.LittleEndian, &msg_head_msg_len)
 			msg_head_msg_len = binary.LittleEndian.Uint32(msg_buff.Next(4))
 			if msg_head_msg_len > 10240 {
 				fmt.Println("too long message")
