86e8bba679313884a50d746010f2e02467d7be11
TonyChen <chenyongsuda@gmail.com>
2016-02-03 17:24:54 +0800

    Create Clientest.go

---
+++ BaseLanguage/GO/ChatServer/src/Clientest.go
@@ -0,0 +1,74 @@
+package main
+
+import (
+	//"bufio"
+	"bytes"
+	"encoding/binary"
+	"fmt"
+	"net"
+	//"os"
+	"time"
+)
+
+const (
+	ADDR = "127.0.0.1:1999"
+)
+
+func main() {
+	conn, err := net.Dial("tcp", ADDR)
+	if err != nil {
+		fmt.Println("连接服务器失败")
+		return
+	}
+	fmt.Println("已经连接服务器")
+	Client(conn)
+	conn.Close()
+}
+
+func Client(conn net.Conn) {
+
+	message := []byte("我是UTF-8")
+	cmd_type := uint32(1)
+	tlen := 4 + 4 + len(message)
+	send_buff := bytes.NewBuffer(make([]byte, 0, 1024))
+
+	for {
+		for i := 0; i < 10; i++ {
+			binary.Write(send_buff, binary.LittleEndian, uint32(len(message)))
+			binary.Write(send_buff, binary.LittleEndian, cmd_type)
+			send_buff.Write(message)
+		}
+
+		fmt.Println("发送一整条信息：")
+		conn.Write(send_buff.Next(tlen))
+		time.Sleep(time.Second)
+
+		//发送不完整head信息
+		fmt.Println("发送不完整head信息：")
+		conn.Write(send_buff.Next(2))
+		time.Sleep(time.Second)
+		conn.Write(send_buff.Next(tlen - 2))
+		time.Sleep(time.Second)
+
+		fmt.Println("发送3条信息：")
+		time.Sleep(time.Second)
+		conn.Write(send_buff.Next(3 * tlen))
+
+		fmt.Println("发送不全的消息体：")
+		time.Sleep(time.Second)
+		conn.Write(send_buff.Next(6))
+		time.Sleep(time.Second)
+		conn.Write(send_buff.Next(tlen - 6))
+
+		fmt.Println("多段发送：")
+		conn.Write(send_buff.Next(tlen + 2))
+		time.Sleep(time.Second)
+		conn.Write(send_buff.Next(-2 + tlen - 8))
+		time.Sleep(time.Second)
+		conn.Write(send_buff.Next(8 + 1))
+		time.Sleep(time.Second)
+		conn.Write(send_buff.Next(-1 + tlen + tlen))
+
+	}
+
+}
