47213c728938ef9b6cef74a49c67587eb4884646
chenyongsuda <chenyongsuda@gmail.com>
2016-09-16 22:36:05 +0800

    1. 添加BootStar项目

---
+++ BootStrap/header.php
@@ -0,0 +1,17 @@
+<div class="row">
+	<div class="col col-lg-12">
+		<div class="brand">
+		<img src="images/xxx" alt="">
+	</div>
+	<div class="menu">
+		<ul class="nav nav-pills">
+		  <li role="presentation" class="active"><a href="#">Home</a></li>
+		  <li role="presentation"><a href="#">Profile</a></li>
+		  <li role="presentation"><a href="#">Messages</a></li>
+		  <li role="presentation"><a href="#">Profile</a></li>
+		  <li role="presentation"><a href="#">Messages</a></li>
+		  <li role="presentation"><a href="#">Profile</a></li>
+		  <li role="presentation"><a href="#">Messages</a></li>
+		</ul>
+	</div>					
+</div>
